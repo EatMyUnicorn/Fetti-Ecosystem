3ecbefa4c54b602ac5d87ecf05f8a848a315c610
eatmyunicorn <eatmyunicorn318@gmail.com>
2022-02-25 01:22:44 -0500

    fixed link again

--- src/logged_out/components/home/HeadSection.js
+++ src/logged_out/components/home/HeadSection.js
@@ -144,16 +144,21 @@ function HeadSection(props) {
                             deed takes in the form of a smart contract. Every Good Deed Deserves Fetti.
                             <p/>
                           </Typography>
+                          <Link
+                            key={element.name}
+                            to="/Whitepaper"
+                            className={classes.noDecoration}
+                          >
                             <Button
                               variant="contained"
                               color="secondary"
                               fullWidth
                               className={classes.extraLargeButton}
                               classes={{ label: classes.extraLargeButtonLabel }}
-                              href="/Whitepaper"
                             >
                               Check out the Whitepaper
                             </Button>
+                          </Link>
                         </Box>
                       </div>
                     </Box>
